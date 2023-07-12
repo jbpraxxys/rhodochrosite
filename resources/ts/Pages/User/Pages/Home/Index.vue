6c7a7f2d9eb5fd09c66106310f3c2175644f16b6
Westine Lao <westine.lao@praxxys.ph>
2023-07-12 15:44:48 +0800

    Update Index.vue

--- resources/ts/Pages/User/Pages/Home/Index.vue
+++ resources/ts/Pages/User/Pages/Home/Index.vue
@@ -2,13 +2,6 @@
     <user-layout>
         <section class="relative h-screen">
             <h1>Laravel</h1>
-
-            <v-password-input
-                name="text-password"
-                id="text-password"
-                placeholder="Sample Placeholder"
-                label="Sample Label"
-            />
         </section>
     </user-layout>
 </template>
