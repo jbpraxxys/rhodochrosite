65adff30986ccd69101964d728fbf455df92fed0
Sitti Dimalna <sitti@praxxys.ph>
2023-09-05 11:09:46 +0800

    wip

--- resources/ts/Components/Partials/Breadcrumbs.vue
+++ resources/ts/Components/Partials/Breadcrumbs.vue
@@ -1,50 +1,30 @@
 <template>
-    <nav class="flex" aria-label="Breadcrumb">
-        <ol class="flex items-center space-x-4">
-            <li>
-                <div>
-                    <Link
-                        :href="route('admin.dashboard')"
-                        class="text-gray-400 hover:text-gray-500"
-                    >
-                        <HomeIcon class="flex-shrink-0 h-5 w-5" aria-hidden="true" />
-                        <span class="sr-only">Home</span>
-                    </Link>
-                </div>
-            </li>
-            <li v-for="page in pages" :key="page.name">
-                <div class="flex items-center">
-                    <ChevronRightIcon
-                        class="flex-shrink-0 h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                    />
-                    <Link
-                        :href="page.href"
-                        class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                        :aria-current="page.current ? 'page' : undefined"
-                        >{{ page.name }}
-                    </Link>
-                </div>
-            </li>
-        </ol>
-    </nav>
+    <div class="flex items-center space-x-4 mb-3">
+        <div v-for="(page, index) in pages" :key="page.name">
+            <div class="flex items-center">
+                <ChevronRightIcon
+                    v-if="parseInt(index) != 0"
+                    class="flex-shrink-0 h-4 w-4 mr-4 text-primary-600"
+                    aria-hidden="true"
+                />
+                <Link
+                    :href="page.href"
+                    class="text-sm text-gray-500 hover:text-gray-700"
+                    :aria-current="page.current ? 'page' : undefined"
+                    >{{ page.name }}
+                </Link>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import { ChevronRightIcon, HomeIcon } from "@heroicons/vue/24/solid";
 import { Link } from "@inertiajs/vue3";
 
-export default {
-    components: {
-        ChevronRightIcon,
-        HomeIcon,
-        Link,
+defineProps({
+    pages: {
+        type: Object,
     },
-    props: {
-        pages: {
-            type: Array,
-            default: [],
-        },
-    },
-};
+})
 </script>
--- resources/ts/Layouts/AdminLayout.vue
+++ resources/ts/Layouts/AdminLayout.vue
@@ -1,13 +1,43 @@
 <template>
     <div class="relative h-screen flex overflow-hidden bg-white dark:bg-gray-800">
 
-        <SidebarMenu
+        <sidebar-menu
         :show="sidebarOpen"
         @close="sidebarOpen = false"/>
 
-        <!-- Main column -->
         <div class="flex flex-col w-0 flex-1 overflow-hidden">
-            <!-- mbl header -->
+            <div class="flex items-center justify-between px-4 py-2 border-b border-gray-100">
+                <div>
+                    <!-- mobile menu -->
+                    <div class="w-10 h-10 p-2.5">
+                        <Bars3BottomLeftIcon class="w-5 h-5"/>
+                    </div>
+                </div>
+                <header-menu />
+            </div>
+            <main class="relative overflow-y-auto focus:outline-none">
+                <div class="flex items-center justify-between py-8 px-12 border-b border-gray-100">
+                    <div>
+                        <breadcrumbs :pages="pages" />
+                        <h1
+                            class="text-3xl font-black truncate"
+                        >
+                            {{ title }}
+                        </h1>
+                    </div>
+                    <div class="flex items-center justify-end space-x-2">
+                        <slot name="actionButtons"/>
+                    </div>
+                </div>
+
+                <!-- Flash Messages -->
+                <flash-messages />
+
+                <slot></slot>
+            </main>
+        </div>
+
+        <!-- <div class="flex flex-col w-0 flex-1 overflow-hidden">
             <div
                 class="relative z-10 flex-shrink-0 flex h-16 bg-white border-b border-gray-200 lg:hidden"
             >
@@ -19,10 +49,8 @@
                     <Bars3CenterLeftIcon class="h-6 w-6" aria-hidden="true" />
                 </button>
             </div>
-            <!--  -->
 
             <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none">
-                <!-- Page title & actions -->
                 <div
                     class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between lg:px-6"
                 >
@@ -45,63 +73,46 @@
                     </div>
                 </div>
 
-                <!-- Flash Messages -->
-                <flash-messages />
-
-                <slot></slot>
+                
             </main>
-        </div>
+        </div> -->
     </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import { router } from "@inertiajs/vue3";
 import { ref, computed } from "vue";
-import { Link } from "@inertiajs/vue3";
 import FlashMessages from "@/Components/Modals/FlashMessages.vue";
 import SidebarMenu from "@/Components/Partials/SidebarMenu.vue";
+import Breadcrumbs from "@/Components/Partials/Breadcrumbs.vue";
 import HeaderMenu from "@/Components/Partials/HeaderMenu.vue";
 
 import {
-    Bars3CenterLeftIcon,
-    ChevronLeftIcon,
+    Bars3BottomLeftIcon,
 } from "@heroicons/vue/24/outline";
 
-export default {
-    components: {
-        Bars3CenterLeftIcon,
-        ChevronLeftIcon,
-        Link,
-        FlashMessages,
-        SidebarMenu,
-        HeaderMenu
-    },
-    props: {
-        title: String,
-        backUrl: {
-            type: String,
-            default: null,
-        },
-        showBack: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    setup(props) {
-        const sidebarOpen = ref(false);
 
-        const canBack = computed(() => props.showBack);
-
-        function goBack() {
-            const url = props.backUrl;
-            router.visit(url);
-        }
-
-        return {
-            sidebarOpen,
-            goBack,
-            canBack,
-        };
+const props = defineProps({
+    title: String,
+    backUrl: {
+        type: String,
+        default: null,
     },
-};
+    showBack: {
+        type: Boolean,
+        default: false,
+    },
+    pages: {
+        type: Object,
+    },
+})
+
+const sidebarOpen = ref(false);
+
+const canBack = computed(() => props.showBack);
+
+const goBack = () => {
+    const url = props.backUrl;
+    router.visit(url);
+}
 </script>
