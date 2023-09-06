80b3311a1e05ae2d22245159a227fe31543cf993
PRAXXYS Labs <praxxyslabs@PRAXXYSs-Mini-2.lan>
2023-09-06 08:26:56 +0800

    Fixed #32

--- resources/ts/Pages/Admin/AdminManagement/View.vue
+++ resources/ts/Pages/Admin/AdminManagement/View.vue
@@ -1,61 +1,33 @@
 <template>
-    <admin-layout :pages="pages" title="Admins">
-        <template #actionButtons>
-            <ExportButton
-                v-if="activeTab !== 'activity_logs'"
-                :routeLink="route('admin.admin-management.index', { action: 'export' })"
-                class="mr-2"
-            />
-            <CreateButton
-                v-if="activeTab !== 'activity_logs'"
-                :routeLink="route('admin.admin-management.create')"
-            />
-        </template>
+    <admin-layout :pages="pages">
+        <div class="w-[552px] mx-auto mt-6 border border-gray-100 rounded-lg ">
+            <div class="p-6 text-sm">
+                <p class="font-semibold">Admin Information</p>
+                <p class="text-gray-500">Relevant Information and other details</p>
 
-        <!-- Tabs -->
-        <Tabs
-            :tabs="tabs"
-            :button-items="true"
-            :active-tab="activeTab"
-            @update:tab="(value: string) => (activeTab = value)"
-            :tab-route="route('admin.admin-management.index')"
-        />
-        <div 
-            v-if="activeTab !== 'activity_logs'" 
-            class="w-[552px] mx-auto mt-6 p-6 text-sm border rounded-lg overflow-hidden"
-        >
-            <p class="font-semibold">Admin Information</p>
-            <p class="text-gray-500">Lorem ipsum dolor sit amet consectetur</p>
-
-            <div class="w-1/2 mt-6 inline-block">
-                <div class="mb-6">
+                <div class="my-6">
                     <p class="text-gray-500">Full Name</p>
                     <p class="text-gray-900">{{ admin.name }}</p>
                 </div>
-                <div class="">
+                <div class="w-1/2 inline-block">
                     <p class="text-gray-500">Email Address</p>
                     <p class="text-gray-900">{{ admin.email }}</p>
                 </div>
-            </div>
-
-            <div class="w-1/2 mt-6 inline-block">
-                <div class="mb-6">
+                <div class="w-1/2 inline-block">
+                    <p class="text-gray-500">Contact Number</p>
+                    <p class="text-gray-900">0900 000 0000</p>
+                </div>
+                <div class="mt-6">
                     <p class="text-gray-500">Role</p>
                     <p class="text-blue-600">Superadmin</p>
                 </div>
-                <div class="">
-                    <p class="text-gray-500">Mobile Number</p>
-                    <p class="text-gray-900">0900 000 0000</p>
-                </div>
             </div>
-
-            <!-- first_name: props.admin.first_name,
-            last_name: props.admin.last_name,
-            email: props.admin.email,
-            title: props.admin.title,
-            role_id: props.admin.role_id -->
+            <div class="py-4 px-6 w-full bg-gray-50 border-t border-gray-100 flex justify-end">
+                <jet-button><a :href="route('admin.admin-management.edit', admin.id)">
+                    Update Information
+                </a></jet-button>
+            </div>
         </div>
-
     </admin-layout>
 </template>
 
@@ -96,36 +68,18 @@ const props = defineProps({
  *----------------*/
 const items = computed(() => props.items);
 
-const activeTab = ref<string>(props.selectedTab);
 const showArchiveModal = ref<boolean>(false);
 const showRestoreModal = ref<boolean>(false);
 const selectedItem = ref<any>(null);
 
-const tabs: { name: string, value?: string, count?: Number }[] = [
-    {
-        name: 'Information',
-        value: null,
-        count: props.informationCount
-    },
-    {
-        name: 'Activity Logs',
-        value: 'activityLogs',
-        count: props.activityLogsCount
-    }
-];
-
 const pages = [
-    {
-        href: route("admin.dashboard.index"),
-        name: "Dashboard",
-    },
     {
         href: route("admin.admin-management.index"),
         name: "Admins",
     },
     {
         href: "",
-        name: props.admin.name,
+        name: "View",
     },
 ];
 
