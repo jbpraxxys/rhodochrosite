94d16fdc5c11f5cb9b5e4ebd8253f1477749b9e9
John Bernardo <john.bernardo@praxxys.ph>
2024-07-02 08:37:12 +0800

    Resolve "CK Editor > Cannot upload webp image"

--- resources/ts/Components/Inputs/CKEditor.vue
+++ resources/ts/Components/Inputs/CKEditor.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        <label :for="id" class="block text-sm text-gray-500 mb-2">{{
-            label
-        }}</label>
+        <label 
+            v-if="label"
+            :for="id" 
+            class="block text-sm text-gray-900 mb-1"
+        >
+            {{ label }}
+        </label>
         <div class="relative">
             <ckeditor
                 :editor="editor"
@@ -11,23 +15,6 @@
                 :config="editorConfig"
                 class="h-40"
             ></ckeditor>
-            <div
-                v-if="error"
-                class="
-                    absolute
-                    inset-y-0
-                    right-0
-                    pr-3
-                    flex
-                    items-center
-                    pointer-events-none
-                "
-            >
-                <ExclamationCircleIcon
-                    class="h-5 w-5 text-red-500"
-                    aria-hidden="true"
-                />
-            </div>
         </div>
         <p
             v-if="error"
@@ -43,22 +30,26 @@
 <style>
 .ck.ck-toolbar,
 .ck.ck-editor__main>.ck-editor__editable:not(.ck-focused) {
-    border-color: #e5e7eb;
+    border-color: #d1d5db;
 }
 
 .ck.ck-toolbar .ck.ck-toolbar__separator {
-    background: #e5e7eb;
+    background: #d1d5db;
 }
 
-.ck-editor {
-    font-size: 14px;
-}
+.ck-editor { font-size: 1rem; }
+.ck-editor h1 { font-size: 2rem; }
+.ck-editor h2 { font-size: 1.875rem; }
+.ck-editor h3 { font-size: 1.5rem; }
+.ck-editor h4 { font-size: 1.25rem; }
+.ck-editor h5 { font-size: 1.125rem; }
+.ck-editor h6 { font-size: 1rem; }
 
 .ck-editor ul {
     display: block;
     list-style-type: disc;
     margin-top: 1em;
-    margin-bottom: 1 em;
+    margin-bottom: 1em;
     margin-left: 0;
     margin-right: 0;
     padding-left: 40px;
@@ -91,93 +82,73 @@
 }
 </style>
 
-<script>
-import { ExclamationCircleIcon } from "@heroicons/vue/24/solid";
+<script setup lang="ts">
+import { ref, computed } from 'vue';
 import CKEditor from "@ckeditor/ckeditor5-vue";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import Editor from 'ckeditor5-custom-build';
 import { UploadAdapter } from '../../plugins/ckeditor/UploadAdapter.ts';
-export default {
-    components: {
-        ExclamationCircleIcon: ExclamationCircleIcon,
-        ckeditor: CKEditor.component,
-    },
-    props: {
-        modelValue: {
-            default: '',
-            required: true,
-        },
-        error: {
-            type: String,
-            default: null,
-        },
-        placeholder: {
-            type: String,
-            default: null,
-        },
-        name: {
-            type: String,
-            default: null,
-        },
-        id: {
-            type: String,
-            default: null,
-        },
-        label: {
-            type: String,
-            default: null,
-        },
-        config: {
-            type: Object,
-            default: {},
-        },
-    },
-    data() {
-        return {
-            editor: ClassicEditor,
-        };
-    },
-    computed: {
-        editorConfig() {
-            return Object.entries(this.config).length
-                ? this.config
-                : {
-                    name: this.name,
-                    height: 500,
-                    mediaEmbed: { previewsInData: true },
-                    extraPlugins: [ function (editor) {
-                        editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-                                return new UploadAdapter(loader);
-                        }
-                    }],
-                    link: {
-                        addTargetToExternalLinks: true,
-                    },
-                    heading: {
-                        options: [
-                            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-                            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-                            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
-                            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
-                            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
-                            {
-                                model: 'small',
-                                view: {
-                                    name: 'h6',
-                                    classes: 'small'
-                                },
-                                title: 'Small',
-                                class: 'ck-heading_heading6_small',
 
-                                // It needs to be converted before the standard 'heading2'.
-                                converterPriority: 'high'
-                            }
-                        ]
-                    }
-                };
-        },
+const props = defineProps({
+    modelValue: {
+        default: '',
+        required: true,
     },
+    error: {
+        type: String,
+        default: null,
+    },
+    placeholder: {
+        type: String,
+        default: null,
+    },
+    name: {
+        type: String,
+        default: null,
+    },
+    id: {
+        type: String,
+        default: null,
+    },
+    label: {
+        type: String,
+        default: null,
+    },
+    config: {
+        type: Object,
+        default: {},
+    },
+});
 
-};
+const editor = ref(Editor);
+const ckeditor = ref(CKEditor.component);
+
+const editorConfig = computed(() => {
+return Object.entries(props.config).length
+    ? props.config
+    : {
+        name: props.name,
+        height: 500,
+        mediaEmbed: { previewsInData: true },
+        extraPlugins: [ function (editor) {
+            editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+            return new UploadAdapter(loader);
+            }
+        }],
+        link: {
+            addTargetToExternalLinks: true,
+        },
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+            ]
+        }
+    };
+});
+
 </script>
